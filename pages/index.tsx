806cbacb87c11a2f7a3feeed42e3351d75fc87c6
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2020-11-16 11:52:40 -0500

    add model-viewer demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,37 @@
+import * as React from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Head>
+      <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+      <script noModule src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"></script>
+      <style>
+        {`
+        .demo {
+          height: 600px;
+          width: 100%;
+        }
+        .demo model-viewer {
+          width: 100%;
+          height: 100%;
+          background-color: #eee;
+        }
+        `}
+      </style>
+    </Head>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
+    <div className="demo">
+      {/* @ts-expect-error */}
+      <model-viewer src="/release-mechanism.glb" alt="A 3D model of an astronaut" auto-rotate camera-controls shadow-intensity="1.0" exposure="0.25"></model-viewer>
+    </div>
   </Layout>
 )
 
